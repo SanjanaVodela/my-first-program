a29cca1611458a73a8be225976ff75438a709f8b
Sanjana Vodela <sanjanavodela1019@gmail.com>
2026-01-16 19:24:37 +0530

    fibonacci series

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i;
+    int f1 = 0, f2 = 1, f3;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci series: ");
+    for (i = 1; i <= n; i++) 
+    {
+        printf("%d ", f1);
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+    }
+    return 0;
+}
