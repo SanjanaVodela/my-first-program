629b2d91dca78e5e9911d3510feac87875789b49
Sanjana Vodela <sanjanavodela1019@gmail.com>
2026-01-16 19:07:36 +0530

    reverse of a number

---
+++ reverseofnumber.c
@@ -0,0 +1,15 @@
+//reverse of a number using while loop
+#include <stdio.h>
+int main ()
+{
+    int n,rev=0,rem;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        rev=(rev*10)+rem;
+        n=n/10;
+    }
+    printf("rev=%d",rev);
+    return 0;
+}
