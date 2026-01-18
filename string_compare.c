c169dfa28f030a404bf72d5c4b6f7a0da587b060
Sanjana Vodela <sanjanavodela1019@gmail.com>
2026-01-18 13:06:58 +0530

    string compair

---
+++ string_compare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    char s1[50], s2[50];
+    printf("Enter first string: ");
+    scanf("%s", s1);
+    printf("Enter second string: ");
+    scanf("%s", s2);
+    if (strcmp(s1, s2) == 0)
+        printf("Strings are equal\n");
+    else
+        printf("Strings are not equal\n");
+    return 0;
+}
