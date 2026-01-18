c0b0e68be3181a3e2d54fee16f90bb8346fcd581
Sanjana Vodela <sanjanavodela1019@gmail.com>
2026-01-18 12:36:35 +0530

    array largest

---
+++ array_largestelement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+   {
+    int a[10], i, n, max;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+    max = a[0];
+    for (i = 1; i < n; i++) 
+    {
+        if (a[i] > max) 
+        {
+            max = a[i];
+        }
+    }
+    printf("Largest element = %d\n", max);
+    return 0;
+}
