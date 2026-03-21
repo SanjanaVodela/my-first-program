1a480f9b3f9edc88e9fa1e871d8cfc2e7eccceb9
Sanjana Vodela <sanjanavodela1019@gmail.com>
2026-03-21 13:36:30 +0530

    lcm.c

--- lcm.c
+++ lcm.c
@@ -4,13 +4,14 @@ int main()
     int a, b, i, lcm;
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-    for (i = 1; ; i++) 
+    while(1)
     {
         if (i % a == 0 && i % b == 0) 
         {
             lcm = i;
             break;
         }
+        i++;
     }
     printf("LCM = %d\n", lcm);
     return 0;
